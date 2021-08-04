de3bb57c67812174f334ee28f9091e36d496f6be
Simon Tessard <simon.tessard@gmail.com>
2021-08-05 01:29:58 +0200

    Added contexte to project + blur effect

--- js/script.js
+++ js/script.js
@@ -1,12 +1,28 @@
 
 function chbg(element) {
-    if (element == 'firstProject') {
-        document.getElementById('secondProject').style.filter = "blur(3px)" }
-    if (element == 'secondProject') { document.getElementById('firstProject').style.filter = "blur(3px)" }
+    if (element == 'firstProject') 
+    {
+        document.getElementById('secondProject').style.filter = "blur(3px)";
+        document.getElementById('thirdProject').style.filter = "blur(3px)";
     }
+    if (element == 'secondProject') 
+    { document.getElementById('firstProject').style.filter = "blur(3px)";
+    document.getElementById('thirdProject').style.filter = "blur(3px)"; 
+    }
+    if (element == 'thirdProject') {
+        document.getElementById('firstProject').style.filter = "blur(3px)";
+        document.getElementById('secondProject').style.filter = "blur(3px)";
+    }
+}
+    
     
 function chbgdefault(element) {
-    if (element == 'firstProject') { document.getElementById('secondProject').style.filter = "grayscale(0)" }
+    if (element == 'firstProject') { document.getElementById('secondProject').style.filter = "grayscale(0)";
+    document.getElementById('thirdProject').style.filter = "grayscale(0)"; }
     
-    if (element == 'secondProject') { document.getElementById('firstProject').style.filter = "grayscale(0)" }
-    }
+    if (element == 'secondProject') { document.getElementById('firstProject').style.filter = "grayscale(0)";
+    document.getElementById('thirdProject').style.filter = "grayscale(0)"; }
+
+    if (element == 'thirdProject') { document.getElementById('firstProject').style.filter = "grayscale(0)";
+    document.getElementById('secondProject').style.filter = "grayscale(0)"; }
+}
